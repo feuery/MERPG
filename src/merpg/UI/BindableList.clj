e3689930cf5d2331d3caf180f5d5a3bba499fa76
Feuer <feuer@mac>
2014-07-30 19:28:52 +0300

    Added loading of tilesets and creation of layers to GUI, added broken support of reverse lists to BindableList and changed most of the refs into atoms because https://twitter.com/Feuer_himbeere/status/494478673945567232

--- src/merpg/UI/BindableList.clj
+++ src/merpg/UI/BindableList.clj
@@ -4,22 +4,27 @@
 
 (defn bindable-list [list-data-atom selected-item-atom &
                      {:keys [custom-model-bind
-                             selected-index-atom]
+                             selected-index-atom
+                             reverse?]
                       :or {custom-model-bind nil
-                           selected-index-atom (atom 0)}}]
+                           selected-index-atom (atom 0)
+                           reverse? false}}]
   (defn create-renderer
     [renderer {:keys [value]}]
     (config! renderer :text (str (custom-model-bind value))))
   
-  (let [list (listbox :model @list-data-atom
+  (let [list (listbox :model (if reverse?
+                               (reverse @list-data-atom)
+                               @list-data-atom)
                       :renderer create-renderer)]
-    (b/bind list-data-atom (b/property list :model))
+    (if reverse?
+      (b/bind list-data-atom
+              (b/transform reverse)
+              (b/property list :model))
+            (b/bind list-data-atom
+              (b/property list :model)))
     (b/bind (b/selection list) selected-item-atom)
     (b/bind (b/selection list) (b/transform (fn [_]
                                               (.getSelectedIndex list)))
             selected-index-atom)
     list))
-
-(defn string-renderer [f]
-  (default-list-cell-renderer
-    (fn [this {:keys [value]}] (.setText this (str (f value))))))
--- src/merpg/UI/main_layout.clj
+++ src/merpg/UI/main_layout.clj
@@ -1,12 +1,15 @@
 (ns merpg.UI.main-layout
-  (:require [seesaw.core :refer :all]
+  (:require [seesaw.core :refer [frame border-panel
+                                 vertical-panel top-bottom-split
+                                 button]]
+            [seesaw.chooser :refer :all]
             [merpg.IO.tileset :refer [load-tileset]]
             [merpg.UI.map-controller :refer [map-controller]]
             [merpg.UI.tileset-controller :refer :all]
             [merpg.UI.tool-box :refer [tool-frame!]]
             [merpg.UI.BindableCanvas :refer :all]
             [merpg.UI.BindableList :refer :all]
-            [merpg.immutable.basic-map-stuff :refer [make-map tile layer-count]]))
+            [merpg.immutable.basic-map-stuff :refer :all]))
 
 (defn get-content []
   (let [map-width  10
@@ -20,18 +23,18 @@
     (def tool-atom (atom {}))
     (def current-tool-fn (atom nil))
     
-    (def tileset-ref (ref [(load-tileset "/Users/feuer2/Dropbox/memapper/tileset.png")]))
-    (def current-tileset-index (ref 0))
-
+    (def tileset-atom (atom [(load-tileset "/Users/feuer2/Dropbox/memapper/tileset.png")]))
+    (def current-tileset-index-atom (atom 0))
+    (def current-tileset-atom (atom nil))
     (def current-tile (ref (tile 0 0 0 0))))
   
   (border-panel
    :center
    (top-bottom-split
-    (map-controller map-data-image tool-atom current-tool-fn tileset-ref
+    (map-controller map-data-image tool-atom current-tool-fn tileset-atom
                     current-tile current-layer-index-atom)
-    (tileset-controller tileset-ref
-                        current-tileset-index
+    (tileset-controller tileset-atom
+                        current-tileset-index-atom
                         current-tile)
     :divider-location 3/4)
    :west (vertical-panel
@@ -39,7 +42,7 @@
           [(tool-frame! tool-atom current-tool-fn)
            (bindable-canvas current-tile
                             (fn [tile]
-                              (-> @tileset-ref
+                              (-> @tileset-atom
                                   (get (:tileset tile))
                                   (get (:x tile))
                                   (get (:y tile)))))
@@ -47,5 +50,30 @@
            (bindable-list map-data-image
                           current-layer-atom
                           :custom-model-bind #(-> % meta :name)
-                          :selected-index-atom current-layer-index-atom)
-           ])))
+                          :selected-index-atom current-layer-index-atom
+                          :reverse? true)
+           (button :text "New layer"
+                   :listen
+                   [:action (fn [_]
+                              (swap! map-data-image conj (make-layer (width @map-data-image)
+                                                                     (height @map-data-image))))])
+           
+           "Tilesets"
+           (bindable-list tileset-atom
+                          current-tileset-atom
+                          :custom-model-bind (fn [_]
+                                               (str (inc @current-tileset-index-atom) "th"))
+                          :selected-index-atom current-tileset-index-atom)
+           (button :text "Load tileset"
+                   :listen
+                   [:action (fn [_]
+                              (choose-file :filters [["Tilesetit" ["png" "jpg" "jpeg"]]]
+                                           :remember-directory? true
+                                           :multi? true :success-fn
+                                           (fn [_ files]
+                                             (let [tilesets (->> files
+                                                                 (map str)
+                                                                 (map load-tileset))]
+                                               (swap! tileset-atom #(vec (concat % tilesets)))))))])
+                                                  
+                                                  ])))

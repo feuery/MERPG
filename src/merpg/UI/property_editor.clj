695dbc15977c1cf8c31dcf276702ca55ecca0606
Feuer <feuer@mac>
2014-08-01 21:11:08 +0300

    Property-editor's index-param shows properties in the correct index on the property-atom. Next to fix the serialization to support :index

--- src/merpg/UI/property_editor.clj
+++ src/merpg/UI/property_editor.clj
@@ -1,47 +1,59 @@
 (ns merpg.UI.property-editor
   (:require [seesaw.core :refer :all]))
 
-(defn property-editor [property-atom & {:keys [with-meta?]
-                                        :or {with-meta? false}}]
+(def not-keyword? #(not (keyword? (second %))))
+
+(defn filter-kws [thing with-meta?]
+  (filter not-keyword? (if with-meta?
+                         (meta thing)
+                         thing)))
+
+(defn property-editor [property-atom & {:keys [with-meta? index]
+                                        :or {with-meta? false
+                                             index nil}}]
   (try
     (frame :size [320 :by 240]
            :title "Propertyeditor"
            :visible? true
            
            :content (grid-panel :columns 2 :items
-                                (-> (map (fn [[key val]]
-                                           (let [serialize (fn [e]
-                                                             (println "Swapping " key " to " (text e) " on " property-atom " with-meta? " with-meta)
-                                                             (if with-meta?
-                                                               (swap! property-atom
-                                                                      (fn [old]
-                                                                        (vary-meta old assoc key 
-                                                                                   (cond
-                                                                                    (and
-                                                                                     (not= (text e) "")
-                                                                                     (number? val)) (Long/parseLong (text e))
-                                                                                     (keyword? val) val
-                                                                                     (= (class val) java.lang.Boolean) (selection e)
-                                                                                     :t (text e)))))
-                                                               (swap! property-atom assoc key
-                                                                      (cond
-                                                                       (and
-                                                                        (not= (text e) "")
-                                                                        (number? val)) (Long/parseLong (text e))
-                                                                        (keyword? val) val
-                                                                        (= (class val) java.lang.Boolean) (selection e)
-                                                                        :t (text e)))))]
-                                             [(str key)
-                                              (if (not= (class val) java.lang.Boolean)
-                                                (text :text (str val)
-                                                      :listen [:document serialize])
-                                                (checkbox :selected? val
-                                                          :listen [:action-performed serialize]))]))
-                                         (filter #(not (keyword? (second %))) (if with-meta?
-                                                                                (meta @property-atom)
-                                                                                @property-atom)))
-                                         flatten
-                                         vec)))
+                                (let [properties (filter-kws
+                                                  (if (nil? index)
+                                                    @property-atom
+                                                    (get @property-atom index))
+                                                  with-meta?)]
+                                  (println "props: " properties)
+                                  (-> (map (fn [[key val]]
+                                             (let [serialize (fn [e]
+                                                               (println "Swapping " key " to " (text e) " on " property-atom " with-meta? " with-meta)
+                                                               (if with-meta?
+                                                                 (swap! property-atom
+                                                                        (fn [old]
+                                                                          (vary-meta old assoc key 
+                                                                                     (cond
+                                                                                      (and
+                                                                                       (not= (text e) "")
+                                                                                       (number? val)) (Long/parseLong (text e))
+                                                                                       (keyword? val) val
+                                                                                       (= (class val) java.lang.Boolean) (selection e)
+                                                                                       :t (text e)))))
+                                                                 (swap! property-atom assoc key
+                                                                        (cond
+                                                                         (and
+                                                                          (not= (text e) "")
+                                                                          (number? val)) (Long/parseLong (text e))
+                                                                          (keyword? val) val
+                                                                          (= (class val) java.lang.Boolean) (selection e)
+                                                                          :t (text e)))))]
+                                               [(str key)
+                                                (if (not= (class val) java.lang.Boolean)
+                                                  (text :text (str val)
+                                                        :listen [:document serialize])
+                                                  (checkbox :selected? val
+                                                            :listen [:action-performed serialize]))]))
+                                           properties)
+                                      flatten
+                                      vec))))
     (catch Exception ex
       (println "Plöp")
       (println ex))))

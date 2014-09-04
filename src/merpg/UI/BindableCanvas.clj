a6cf65037fde4be5f9ba1d24805f6834ce91cca0
Feuer <feuer@arch>
2014-09-04 19:58:23 +0300

    Bindablecanvas has been multithreaded. I think it breaks stuff.

--- src/merpg/UI/BindableCanvas.clj
+++ src/merpg/UI/BindableCanvas.clj
@@ -1,7 +1,9 @@
 (ns merpg.UI.BindableCanvas
   (:require [seesaw.core :refer :all]
             [merpg.IO.tileset :refer :all]
-            [clojure.java.io :refer [file]])
+            [clojure.java.io :refer [file]]
+            [clojure.stacktrace :refer [print-stack-trace]]
+            [clojure.pprint :refer [pprint]])
   (:import [javax.imageio ImageIO]))
 
 (defn- save-img [img path]
@@ -21,10 +23,15 @@
   (let [img-provider (atom (future (img-transformer-fn @data-atom)))
         old-image (atom nil)
         c (canvas :paint (fn [_ g]
-                           (when (or (realized? @img-provider)
+                           (try
+                             (when (or (realized? @img-provider)
                                      (nil? @old-image))
                              (reset! old-image @@img-provider)
                              (reset! img-provider (future (img-transformer-fn @data-atom))))
+                             (catch Exception ex
+                               (println "Image provider (" (class @@img-provider) "): ")
+                               (pprint @img-provider)
+                               (print-stack-trace ex)))
 
                            (when-not (nil? @old-image)
                              (.drawImage g @old-image 0 0 nil))))]
@@ -32,7 +39,9 @@
     (doseq [d rest-to-bind]
       (add-watch d :bindable-canvas-updater (fn [_ _ _ _]
                                               (repaint! c))))
-    (add-watch data-atom :bindable-canvas-updater (fn [_ _ _ _]
+    (add-watch data-atom :bindable-canvas-updater (fn [_ _ _ da]
+                                                    (println "Data-atomn of bindable-canvas changed")
+                                                    (pprint da)
                                                     (repaint! c)))
     c))
 

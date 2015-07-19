25cf024e2a0c6c516dd17e63f03a4b8dfd308faf
Feuer <feuer@arch>
2015-07-20 00:08:19 +0300

    Loading dimension-not-divisible-by-50 - bug fixed

--- src/merpg/IO/tileset.clj
+++ src/merpg/IO/tileset.clj
@@ -1,14 +1,18 @@
 (ns merpg.IO.tileset
-  (:require [merpg.2D.core :refer :all]))
+  (:require [merpg.2D.core :refer :all]
+            [clojure.pprint :refer :all]))
 
 (defn img-to-tileset [img]
-  (let [partition-limit (/ (img-height img) 50)
+  (let [partition-limit (long (/ (img-height img) 50))
+        [w h] [(img-width img) (img-height img)]
         coordinate-pairs (for [x (range 0 (img-width img) 50)
-                               y (range 0 (img-height img) 50)]
+                               y (range 0 (img-height img) 50)
+                               :when (and (<= (+ x 50) w)
+                                          (<= (+ y 50) h))]
                            [x y])]
     (->> coordinate-pairs
          (map (fn [[x y]]
-           (subimage img x y 50 50)))
+                (subimage img x y 50 50)))
          (partition partition-limit)
          (map vec)
          vec)))
@@ -18,16 +22,20 @@
 
 (defn tileset-to-img [tileset]
   (if-not (nil? tileset)
-    (let [width (count tileset)
-          height (count (first tileset))
-          img-w (* width 50)
-          img-h (* 50 height)
-          surface (image img-w img-h)]
-      (draw-to-surface surface
-                       (doseq [x-y (for [x (range 0 img-w 50)
-                                         y (range 0 img-h 50)]
-                                     [x y])]
-                         ;; (println "x-y " x-y)
-                         ;; (println "class " (class (get-in tileset (map #(/ % 50) x-y) x-y)))
-                         (Draw (get-in tileset (map #(/ % 50) x-y)) x-y))))
+    (try
+      (let [width (count tileset)
+            height (count (first tileset))
+            img-w (* width 50)
+            img-h (* 50 height)
+            surface (image img-w img-h)]
+        (draw-to-surface surface
+                         (doseq [x-y (for [x (range 0 img-w 50)
+                                           y (range 0 img-h 50)]
+                                       [x y])]
+                           ;; (println "x-y " x-y)
+                           ;; (println "class " (class (get-in tileset (map #(/ % 50) x-y) x-y)))
+                           (Draw (get-in tileset (map #(/ % 50) x-y)) x-y))))
+      (catch IllegalArgumentException ex
+        (pprint tileset)
+        (throw ex)))
     (image 50 50)))

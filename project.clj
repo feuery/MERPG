14504075c67f8c13aef8b325b2cb7592c878d5e1
Ilpo Lehtinen <feuer@roland.home>
2016-06-05 00:56:29 +0300

    Trying to re-setup magit... :D

--- project.clj
+++ project.clj
@@ -3,11 +3,12 @@
   :url "http://yearofourlord.blogspot.com"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [seesaw "1.4.4"]
                  [environ "0.5.0"]
-                 [org.clojure/tools.nrepl "0.2.12"]]
-  :plugins [[cider/cider-nrepl "0.10.2"]]
+                 ;; [org.clojure/tools.nrepl "0.2.12"]
+                 ]
+  ;; :plugins [[cider/cider-nrepl "0.10.2"]]
   :main merpg.core
   :java-source-paths ["java-src"]
   :profiles {:uberjar {:aot :all}}

6f1c45c71acf9d0bca57dc09bb13ad3f34114634
Ilpo Lehtinen <feuer@roland.home>
2016-07-21 22:18:43 +0300

    We have a minor mode

--- cider-merpg/cider-merpg.el
+++ cider-merpg/cider-merpg.el
@@ -5,29 +5,39 @@
 
 (setq lexical-binding t)
 
+(make-variable-buffer-local (defvar ns "" "Buffer-local var to track the ns of the currently opened merpg script asset's buffer"))
+
+(replace-regexp-in-string "^!\\(.*\\)" "\\1" "!merpg.core.lol")
+
 (defun merpg-find-file (url)
-  (interactive "sFind ns: ")
-  (message (concat "Finding file from ns " url "..."))
-  (cider-nrepl-send-request (list "op" "find-file"
-				  "ns" url
-				  "debug?" (if *merpg-debug*
-					       "true"
-					     "false"))
-			    (lambda (result)
-			      (message (concat "Found ns " url ", opening..."))
-			      
-			      (let* ((buf-name (concat "MERPG: " url))
-				     (buf (get-buffer-create buf-name)))
-				(set-buffer buf)
-				(insert (nrepl-dict-get result "contents"))
-				(switch-to-buffer buf)
-				(clojure-mode)
-				;; (enable cider-merpg-mode)
-				(message (nrepl-dict-get result "notes"))))))
-				;; (switch-to-buffer buf)
+  (interactive "sFind file: ")
+  (if (string-prefix-p "!" url)
+      (let ((real-url (replace-regexp-in-string "^!\\(.*\\)" "\\1" url)))
+	(message (concat "Finding file from ns " real-url "..."))
+	(cider-nrepl-send-request (list "op" "find-file"
+					"ns" real-url
+					"debug?" (if *merpg-debug*
+						     "true"
+						   "false"))
+				  (lambda (result)
+				    (message (concat "Found ns " real-url ", opening..."))
+				    
+				    (let* ((buf-name (concat "MERPG: " real-url))
+					   (buf (get-buffer-create buf-name)))
+				      (set-buffer buf)
+				      (insert (nrepl-dict-get result "contents"))
+				      (switch-to-buffer buf)
+				      (setq ns real-url)				
+				      (clojure-mode)
+				      ;; (enable cider-merpg-mode)
+				      (message (nrepl-dict-get result "notes"))))))
+    (find-file url)))
 
-			      ;; (message (concat "Got result " (prin1-to-string result))))))
-
-;; Debugger entered--Lisp error: (void-variable n)
- 
-(merpg-find-file "merpg.core.lol")
+(define-minor-mode merpg-edit-mode
+  "An extension to CIDER. With cider connected to a live instance of MERPG (URL `https://github.com/feuery/memapper/'), this mode overrides C-x C-f and C-x C-s to be able to load and save script assets from the game"
+  :lighter " merpg"
+  :keymap (let ((map (make-sparse-keymap)))
+	    (define-key map (kbd "C-x C-f") 'merpg-find-file)
+	    map)
+  :global nil)
+  
